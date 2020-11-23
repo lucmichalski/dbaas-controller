a4445da736769e037ef3b6e7cfc207da50b2e602
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-11-23 18:49:05 +0300

    Limit resources for PMM pods.

--- service/k8sclient/k8client_test.go
+++ service/k8sclient/k8client_test.go
@@ -58,11 +58,15 @@ func TestK8Client(t *testing.T) {
 		l.Info("No XtraDB Clusters running")
 
 		err = client.CreateXtraDBCluster(ctx, &XtraDBParams{
-			Name:                name,
-			Size:                2,
-			PXC:                 &PXC{DiskSize: 1024 * 1024 * 1024},
-			ProxySQL:            &ProxySQL{DiskSize: 1024 * 1024 * 1024},
-			PMMPublicAddressURL: "127.0.0.1",
+			Name: name,
+			Size: 1,
+			PXC: &PXC{
+				DiskSize: 5 * 1024 * 1024 * 1024,
+			},
+			ProxySQL: &ProxySQL{
+				DiskSize: 5 * 1024 * 1024 * 1024,
+			},
+			PMMPublicAddressURL: "host.minikube.internal",
 		})
 		require.NoError(t, err)
 
@@ -118,10 +122,12 @@ func TestK8Client(t *testing.T) {
 		l.Info("No PSMDB Clusters running")
 
 		err = client.CreatePSMDBCluster(ctx, &PSMDBParams{
-			Name:                name,
-			Size:                3,
-			Replicaset:          &Replicaset{DiskSize: 1024 * 1024 * 1024},
-			PMMPublicAddressURL: "127.0.0.1",
+			Name: name,
+			Size: 3,
+			Replicaset: &Replicaset{
+				DiskSize: 1024 * 1024 * 1024,
+			},
+			PMMPublicAddressURL: "host.minikube.internal",
 		})
 		require.NoError(t, err)
 
